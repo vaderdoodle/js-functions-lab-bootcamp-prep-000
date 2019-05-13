3a12078f78456a594cd148ef8e382a24a77df87e
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-13 19:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo (holiday, name) {
-  return `Happy ${holiday},`;
+  return `Happy ${holiday}, ${name}`;
 }
 
 function holidayCountdown (holiday, days) {
